0a66008f1982306b798678847d0db2c5129425f3
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-07-19 12:55:08 +0900

    Create 1260_DFS&BFS.py

---
+++ Silver/1260_DFS&BFS.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def dfs(start): #dfs 정의
+    visits[start] = True
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if not visits[neighbor]:
+            dfs(neighbor)
+
+def bfs(start):
+    queue = deque([start])
+    visits[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end=" ")
+
+        for i in graph[v]:
+            if not visits[i]:
+                queue.append(i)
+                visits[i] = True
+
+n, m, s = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+graph = [sorted(neighbors) for neighbors in graph]
+
+visits = [False for _ in range(n+1)]
+dfs(s)
+print()
+visits = [False for _ in range(n+1)]
+bfs(s)
