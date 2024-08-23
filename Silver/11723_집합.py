ac5abaf218ec3fa12c7aa9866d3badee9977c924
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-08-23 09:49:23 +0900

    :white_check_mark: 11723_집합

--- Silver/11723_집합.py
+++ Silver/11723_집합.py
@@ -12,9 +12,33 @@ empty: S를 공집합으로 바꾼다.
 
 각각의 케이스를 나누어서
 '''
-
 import sys
-import heapq
 
 N = int(sys.stdin.readline())
+S = set()
 
+for i in range(N):
+    cmd_and_x = sys.stdin.readline().split()
+    if len(cmd_and_x) == 1:
+        if cmd_and_x[0] == 'all':
+            S = set([x for x in range(1, 21)])
+        else:
+            S = set()
+
+    else:
+        cmd, x = cmd_and_x[0], int(cmd_and_x[1])
+        if cmd == 'add':
+            S.add(x)
+        elif cmd == 'check':
+            if x in S:
+                print(1)
+            else:
+                print(0)
+        elif cmd == 'remove':
+            if x in S:
+                S.discard(x)
+        elif cmd == 'toggle':
+            if x in S:
+                S.discard(x)
+            else:
+                S.add(x)
