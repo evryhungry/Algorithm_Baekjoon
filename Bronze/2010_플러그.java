c8cb0d2550ad3dab6bc9ca9d9e8632b4ddc8e4e4
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-12 16:49:51 +0900

    Create 2010_플러그.java

---
+++ Bronze/2010_플러그.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+// https://www.acmicpc.net/problem/2010
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+
+        bw.write(String.valueOf(sum-(n-1)) + "\n");
+
+        bw.flush();
+        bw.close();
+    }
+}
