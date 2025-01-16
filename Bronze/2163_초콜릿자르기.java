0b441fe39c788d1e07ac9b206ae54253871b96df
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-16 10:45:12 +0900

    Create 2163_초콜릿자르기.java

---
+++ Bronze/2163_초콜릿자르기.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+// https://www.acmicpc.net/problem/2163
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+
+        System.out.printf("%d\n", m * n - 1);
+    }
+}
