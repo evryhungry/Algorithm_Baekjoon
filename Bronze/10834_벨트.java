a0d9e69c329149b3699a68fd0f4867197cd3873f
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-01-31 23:23:01 +0900

    Update 10834_벨트.java

--- Bronze/10834_벨트.java
+++ Bronze/10834_벨트.java
@@ -6,20 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int rotation = 0;
-        int cycle = 1;
+        int N = 1000 - Integer.parseInt(br.readLine());
+        int[] en = {500, 100, 10, 5, 1};
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            if (c == 1) rotation = 1 - rotation;
-            cycle = cycle / a * b;
-
+        int ans = 0;
+        for (int i = 0; i < en.length; i++) {
+            int cnt = N / en[i];
+            if (cnt > 0) {
+                ans += cnt;
+                N -= cnt * en[i];
+            }
         }
-        System.out.println(rotation + " " + cycle);
     }
 }
