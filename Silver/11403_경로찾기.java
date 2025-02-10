9bd3e1961703e40f7a391471541f12a55e6bde5c
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-10 10:12:10 +0900

    Create 11403_경로찾기.java

---
+++ Silver/11403_경로찾기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+// https://www.acmicpc.net/problem/11403
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (matrix[i][k] == 1 && matrix[k][j] == 1) {
+                        matrix[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(matrix[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        br.close();
+
+    }
+}
+
