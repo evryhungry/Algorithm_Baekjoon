be577c8db25a2b99fdc3d722f672724d0d5099a2
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-02-26 23:08:40 +0900

    Create 5337_웰컴!.java

---
+++ Bronze/5337_웰컴!.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+// https://www.acmicpc.net/problem/5337
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
