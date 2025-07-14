1305ac865766fa1a1bcf9891874f3d50f1126612
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-07-14 16:50:52 +0900

    Update 4659_비밀번호발음하기.java

--- Silver/4659_비밀번호발음하기.java
+++ Silver/4659_비밀번호발음하기.java
@@ -4,53 +4,38 @@ import java.io.*;
 // https://www.acmicpc.net/problem/4659
 // 1600 - 1618
 public class Main {
-    static char[] vowels = {'a', 'e', 'i', 'o', 'u'};
     static boolean isVowel(char c){
-        for (char vowel : vowels){
-            if (c == vowel){
-                return true;
-            }
-        }
-        return false;
+        return c == 'a' || c == 'e' || c == 'o' || c == 'u' || c == 'i';
     }
-    static boolean hasVowel = false;
-    static char prevChar = 0;
-    static int vowelCount = 0;
-    static int consonantCount = 0;
+    static boolean isValid;
+    static char prevChar = '.';
+    static int count ;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringBuilder sb = new StringBuilder();
-        while (true) {
-            boolean isValid = true;
-            String line = br.readLine();
-            if (line.equals("end")) break;
+        String line;
+        while (!(line = br.readLine()).equals("end")) {
+            isValid = false;
+            count = 0;
 
             for (char c : line.toCharArray()) {
-                if (prevChar == c) {
-                    if (c != 'e' && c != 'o') {
-                        isValid = false;
-                        break;
-                    }
-                }
-                if (isVowel(c)) {
-                    hasVowel = true;
-                    consonantCount = 0;
-                    vowelCount++;
-                } else {
-                    vowelCount = 0;
-                    consonantCount++;
-                }
-                if (vowelCount >= 3 || consonantCount >= 3) {
+                if (isVowel(c)) isValid = true;
+
+                if (isVowel(c) != isVowel(prevChar)) count = 1;
+                else count++;
+
+                if (count > 2 || (prevChar == c && (prevChar != 'e' && prevChar != 'o'))){
                     isValid = false;
                     break;
                 }
+
                 prevChar = c;
             }
 
-            if (isValid && hasVowel) {
+            if (isValid) {
                 sb.append("<" + line + "> is acceptable.\n");
             } else {
                 sb.append("<" + line + "> is not acceptable.\n");
