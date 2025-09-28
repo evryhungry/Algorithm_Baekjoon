fed2c68350843a701d10f0ebbfaf02c2ff423185
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-09-28 21:45:04 +0900

    Create 숫자_문자열과_영단어.java

---
+++ programmers/숫자_문자열과_영단어.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String s) {
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for (int i = 0; i < num.length ; i++) s = s.replaceAll(num[i], Integer.toString(i));
+
+        return Integer.parseInt(s);
+    }
+}
