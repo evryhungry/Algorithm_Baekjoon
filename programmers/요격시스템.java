b92b8746832a6e37901b577a4577095994a70589
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2026-03-24 22:17:37 +0900

    Update 요격시스템.java

--- programmers/요격시스템.java
+++ programmers/요격시스템.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[][] targets) {
 
-        Arrays.sort(targets,(o1,o2) -> o1[1]-o2[1]); // 
+        Arrays.sort(targets,(o1,o2) -> o1[1]-o2[1]); // 각 행의 두 번째 원소를 기준으로 오름차순 정렬
 
         int end = 0; int answer = 0 ;
 
