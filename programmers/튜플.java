d0ed257bf916a5f4437b3da68e33829c3686988d
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2026-08-24 14:44:17 +0900

    Update 튜플.java

--- programmers/튜플.java
+++ programmers/튜플.java
@@ -15,8 +15,8 @@
  * 3. 정렬된 집합을 순회하면서, 이전에 등장하지 않은 원소를 결과 리스트에 추가합니다.
  * 
  * 풀고나서 아쉬운점
- * 1. 문자열 파싱 과정에서 정규식을 사용하여 더 간결하게 처리할 수 있었을 것 같습니다. // s.substring(2, s.length() - 2).split("\\},\\{") 처럼 한줄로 처리할 수 있었을 것 같습니다. 
- * 2. int[] answer = new int[answer.size()]; 부분으로 처리 후, groups를 index로 접근하여 처리할 수 있었을 것 같습니다. 
+ * 1. 문자열 파싱 과정에서 정규식을 사용하여 더 간결하게 처리할 수 있었을 것 같다. // s.substring(2, s.length() - 2).split("\\},\\{") 한툴처리 가능한데.. 아숩 
+ * 2. int[] answer = new int[answer.size()]; 부분으로 처리 후, groups를 index로 접근하여 처리해도 괜찮지 않았을까?
  *    // for (int i = 0; i < answer.size(); i++) { result[i] = answer.get(i); } -> for (int i = 0; i < groups.length; i++) { answer[i] = Integer.parseInt(groups[i]); } 
  * 3. Comparator 의 사용을 더 기억해야할 것 같다.
  * 4. 람다식 사용을 더 기억해야할 것 같다. // 적용하는데 조금 시간이 걸린다는 점이 아쉽습니다.
