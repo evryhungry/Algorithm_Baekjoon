e888931933e56741c677ccaabdedb377c2afbca9
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2026-09-22 16:03:17 +0900

    20260922

---
+++ sql/모든레코드조회.sql
@@ -0,0 +1,4 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59034
+SELECT ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE
+from ANIMAL_INS
+order by ANIMAL_ID asc
